807c50a86327437083c4872956ff4d97006ca4fc
johnolivercr <j.quesada.chaves@gmail.com>
2023-09-08 21:05:20 -0600

    Update Ejercicio01.sql

--- SQL/Ejercicio01.sql
+++ SQL/Ejercicio01.sql
@@ -1,7 +1,23 @@
 Use Tienda
 go
 
+-- Paso 1: 
+-- Crear base de datos Tienda
+CREATE DATABASE [Tienda]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'Tienda', FILENAME = N'C:\Backup\Tienda.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'Tienda_log', FILENAME = N'C:\Backup\Tienda_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT, LEDGER = OFF
+GO
 
+-- Paso 2: 
+-- Crear una tabla llamada "Productos" con las siguientes columnas:
+-- ID (Clave primaria)
+-- Nombre
+-- Precio
+-- Stock
 CREATE TABLE [dbo].[Productos] (
     [ID]     INT             IDENTITY (1, 1) NOT NULL,
     [Nombre] NVARCHAR (50)   NOT NULL,
